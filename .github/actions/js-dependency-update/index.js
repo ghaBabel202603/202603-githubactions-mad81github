09b2c2c34c71a3840470090d715a9743f19969eb
Miguel Ángel Dávila Dávila <babel.mdavila@iberia.es>
2026-03-23 17:55:44 +0100

    add index.js

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,7 +1,48 @@
-const core = require('@actions/core');
-async function run() {
-    // Escritura en la salida de la acción
-  core.info('I am a custom JS action');  
-}
-// Ejecuta la función run
-run();
+ const core = require('@actions/core');
+ async function run() {
+     const baseBranch = core.getInput('base-branch');
+     const targetBranch = core.getInput('target-branch');
+     const workingDirectory = core.getInput('working-directory');
+     const ghToken = core.getInput('gh-token');
+     const debug = core.getBooleanInput('debug');
+
+
+
+     core.info(`Base branch: ${baseBranch}`);
+     core.info(`Target branch: ${targetBranch}`);
+     core.info(`Working directory: ${workingDirectory}`);
+
+     const exec = require('@actions/exec');
+     await exec.exec('npm', ['update'], { cwd: workingDirectory });
+
+     let gitStatusOutput = await exec.getExecOutput('git', ['status', '-s', 'package*.json'], { cwd: workingDirectory });
+
+     if (gitStatusOutput.stdout) {
+        core.info('There are updates available.');
+        // Al principio del archivo, importa el paquete @actions/github
+        const github = require('@actions/github');
+        // Código restante
+        const octokit = github.getOctokit(ghToken);
+        try {
+            await octokit.rest.pulls.create({
+                owner: github.context.repo.owner,
+                repo: github.context.repo.repo,
+                title: `Update NPM dependencies`,
+                body: `This pull request updates NPM packages`,
+                base: baseBranch,
+                head: targetBranch
+            });
+        } catch (e) {
+            core.error('[js-dependency-update] : Something went wrong while creating the PR. Check logs below.');
+            core.setFailed(e.message);
+            core.error(e);
+        }
+     } else {
+        core.info('There are no updates at this point in time.');
+     }
+
+     
+
+ }
+ // Ejecuta la función run
+ run();
